ee34bb3cf310c26514a0f9b796b18857085ba84e
Chazz0606 <cklibrary2@qq.com>
2023-09-12 10:32:06 +0800

    input and output using txt file use dictionaries to organize data

--- GPA calculator.py
+++ GPA calculator.py
@@ -1,30 +1,28 @@
-# function to input and standardize score
-def inputScr(sbj):
-    scr=input(sbj+" score:")
-    while scr.isdigit()==False:
-        scr=input("Please enter again:")
-    scr=int(scr)
-    if scr>=92.5:
+#function to input and standardize score
+def stdScr(sbj,scr):
+    while not str(scr).isdigit():
+        scr=input("Please enter "+sbj+" score again:")
+    scr=float(scr)
+    if scr>=93:
         scr=0
-    elif scr>=87.5:
+    elif scr>=88:
         scr=-1
-    elif scr>=82.5:
+    elif scr>=83:
         scr=-2
-    elif scr>=77.5:
+    elif scr>=78:
         scr=-3
-    elif scr>=72.5:
+    elif scr>=73:
         scr=-4
-    elif scr>=67.5:
+    elif scr>=68:
         scr=-5
-    elif scr>=59.5:
+    elif scr>=60:
         scr=-6
     else:
         scr=-7
     return scr
 
-# function to input and standardize level
-def inputLv(sbj):
-    lv=input(sbj+" level:")
+#function to input and standardize level
+def stdLv(sbj,lv):
     if lv=="H+" or lv=="h+":
         lv="H+"
     elif lv=="H" or lv=="h" or lv=="HLM" or lv=="hlm" or lv=="素养班" or lv=="素养":
@@ -35,12 +33,14 @@ def inputLv(sbj):
         lv="S"
     else:
         while lv!="S" and lv!="s" and lv!="S+" and lv!="s+" and lv!="H" and lv!="h" and lv!="素养班" and lv!="素养"and lv!="HLM" and lv!="hlm" and lv!="H+" and lv!="h+" and lv!="non-native" and lv!="nonnative" and lv!="Nonnative" and lv!="Non-native" and lv!="Non-Native":
-            lv=input("Please enter again:")
+            lv=input("Please enter "+sbj+" level again:")
     return lv
 
 #function to standardize level
-def stdLv(lv):
-    if lv=="H":
+def digitLv(lv):
+    if lv=="H+":
+        return 1
+    elif lv=="H":
         return 0
     elif lv=="S+":
         return -1
@@ -54,71 +54,94 @@ def sbjGpa(scr,lv):
     else:
         return 4.3+0.15*lv+0.3*scr
 
-#input scores
-chiScr=inputScr("Chinese")
-mScr=inputScr("Math")
-eScr=inputScr("English")
-hScr=inputScr("History")
-pScr=inputScr("Physics")
-cheScr=inputScr("Chemistry")
-elScr=inputScr("Elective")
+#create dictionaries
+chinese={}
+math={}
+english={}
+history={}
+physics={}
+chemistry={}
+elective={}
 
-#input levels
-chiLv=inputLv("Chinese")
-mLv=inputLv("Math")
-eLv=inputLv("English")
-hLv=inputLv("History")
-pLv=inputLv("Physics")
-cheLv=inputLv("Chemistry")
-elLv=inputLv("Elective")
+#read info file
+file=open("GPA info.txt","r")
+info=file.readlines()
+file.close()
+for sbjNum in range(7):
+    info[sbjNum]=info[sbjNum].split("\t")
+    if info[sbjNum][3]=="":
+        del info[sbjNum][3]
+    for infoNum in range(4):
+        info[sbjNum][infoNum]=info[sbjNum][infoNum].split()[1]
 
-#class times
-chiClsTime=3
-mClsTime=6
-eClsTime=6.5
-hClsTime=4
-pClsTime=3
-cheClsTime=3
-elClsTime=3
+#input info into dictionary
+chinese["level"],chinese["score"],chinese["classTime"]=info[0][1],int(info[0][2]),float(info[0][3])
+math["level"],math["score"],math["classTime"]=info[1][1],int(info[1][2]),float(info[1][3])
+english["level"],english["score"],english["classTime"]=info[2][1],int(info[2][2]),float(info[2][3])
+history["level"],history["score"],history["classTime"]=info[3][1],int(info[3][2]),float(info[3][3])
+physics["level"],physics["score"],physics["classTime"]=info[4][1],int(info[4][2]),float(info[4][3])
+chemistry["level"],chemistry["score"],chemistry["classTime"]=info[5][1],int(info[5][2]),float(info[5][3])
+elective["level"],elective["score"],elective["classTime"]=info[6][1],int(info[6][2]),float(info[6][3])
 
 #standardize level
-if chiLv=="H":
-    chiLv=0
+chinese["level"]=stdLv("Chinese",chinese["level"])
+math["level"]=stdLv("Math",math["level"])
+english["level"]=stdLv("English",english["level"])
+history["level"]=stdLv("History",history["level"])
+physics["level"]=stdLv("Physics",physics["level"])
+chemistry["level"]=stdLv("Chemistry",chemistry["level"])
+elective["level"]=stdLv("Elective",elective["level"])
+
+#standardize score
+chinese["score"]=stdScr("Chinese",chinese["score"])
+math["score"]=stdScr("Math",math["score"])
+english["score"]=stdScr("English",english["score"])
+history["score"]=stdScr("History",history["score"])
+physics["score"]=stdScr("Physics",physics["score"])
+chemistry["score"]=stdScr("Chemistry",chemistry["score"])
+elective["score"]=stdScr("Elective",elective["score"])
+
+#standardize level
+if chinese["level"]=="H":
+    chinese["level"]=0
 else:
-    chiLv=-1
+    chinese["level"]=-1
 
-if eLv=="H+":
-    eLv=1
-elif eLv=="H":
-    eLv=0
-elif eLv=="S+":
-    eLv=-1
+if elective["level"]=="H":
+    elective["level"]=0
 else:
-    eLv=-2
+    elective["level"]=-1
 
-if elLv=="H":
-    elLv=0
-elif elLv=="S":
-    elLv=-1
-
-mLv=stdLv(mLv)
-hLv=stdLv(hLv)
-pLv=stdLv(pLv)
-cheLv=stdLv(cheLv)
+math["level"]=digitLv(math["level"])
+english["level"]=digitLv(english["level"])
+history["level"]=digitLv(history["level"])
+physics["level"]=digitLv(physics["level"])
+chemistry["level"]=digitLv(chemistry["level"])
 
 #find GPA for each subject
-if chiScr==-7:
-    chiGpa=0
+if chinese["score"]==-7:
+    chinese["gpa"]=0
 else:
-    chiGpa=4.3+0.1*chiLv+0.3*chiScr
-mGpa=sbjGpa(mScr,mLv)
-eGpa=sbjGpa(eScr,eLv)
-hGpa=sbjGpa(hScr,hLv)
-pGpa=sbjGpa(pScr,pLv)
-cheGpa=sbjGpa(cheScr,cheLv)
-elGpa=sbjGpa(elScr,elLv)
+    chinese["gpa"]=4.3+0.1*chinese["level"]+0.3*chinese["score"]
+    
+if elective["score"]==-7:
+    elective["gpa"]=0
+else:
+    elective["gpa"]=4.3+0.1*elective["level"]+0.3*elective["score"]
+    
+math["gpa"]=sbjGpa(math["score"],math["level"])
+english["gpa"]=sbjGpa(english["score"],english["level"])
+history["gpa"]=sbjGpa(history["score"],history["level"])
+physics["gpa"]=sbjGpa(physics["score"],physics["level"])
+chemistry["gpa"]=sbjGpa(chemistry["score"],chemistry["level"])
 
 #final result
-gpa=(chiGpa*chiClsTime+mGpa*mClsTime+eGpa*eClsTime+hGpa*hClsTime+pGpa*pClsTime+cheGpa*cheClsTime+elGpa*elClsTime)/(chiClsTime+mClsTime+eClsTime+hClsTime+pClsTime+cheClsTime+elClsTime)
+gpa=chinese["gpa"]*chinese["classTime"]+math["gpa"]*math["classTime"]+english["gpa"]*english["classTime"]+history["gpa"]*history["classTime"]+physics["gpa"]*physics["classTime"]+chemistry["gpa"]*chemistry["classTime"]+elective["gpa"]*elective["classTime"]
+gpa=gpa/(chinese["classTime"]+math["classTime"]+english["classTime"]+history["classTime"]+physics["classTime"]+chemistry["classTime"]+elective["classTime"])
 gpa=int(gpa*1000)/1000
-print("Your GPA is: "+str(gpa))
+print("Your GPA: "+str(gpa))
+
+#put GPA in file
+file=open("GPA result.txt","w")
+file.write("GPA: "+str(gpa))
+file.close()
