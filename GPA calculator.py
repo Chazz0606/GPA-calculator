6e4918afd3d0626abcc243a96c2238dde051958a
Chazz0606 <cklibrary2@qq.com>
2023-09-13 11:48:17 +0800

    add numpy applications change code structure remove comments

--- GPA calculator.py
+++ GPA calculator.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import tkinter as tk
+import numpy as np
 from tkinter import filedialog
 import time
 
 
-# function to input and standardize score
-def stdScr(scr):
+def standardize_score(scr):
     if scr >= 92.5:
         return 1
     elif scr >= 87.5:
@@ -24,8 +24,7 @@ def stdScr(scr):
         return 8
 
 
-# function to input and standardize level
-def stdLv(lv):
+def standardize_level(lv):
     if lv == "HLM" or lv == "素养班":
         return "H"
     if lv == "II":
@@ -37,8 +36,7 @@ def stdLv(lv):
     return lv.upper()
 
 
-# find subject GPA in dictionary
-def findGpa(sbj, lv, scr):
+def find_gpa(sbj, lv, scr):
     if sbj == "Chinese":
         return chineseGpa[lv][scr]
     elif sbj == "English":
@@ -47,100 +45,65 @@ def findGpa(sbj, lv, scr):
         return nonLanguageGpa[lv][scr]
 
 
-# use file dialog to get path
-def getFile():
-    path = tk.filedialog.askopenfilename(initialdir=".\\",
+def open_input_file():
+    path = tk.filedialog.askopenfilename(initialfile='GPA info.xlsx',
                                          title="Select an Excel file",
                                          filetypes=(("Microsoft Excel", "*.xlsx"),
-                                                    ("all files", "*.*")))
-    return path
-
-
-# change entry contents
-def changeEty(info):
-    lst = []
-    for i in range(len(widgetGroup.columns)):
-        if widgetGroup.columns[i][-3:] == "Ety":
-            lst.append(i)
-    for i in range(len(widgetGroup.index)):
-        for j in range(len(lst)):
-            widgetGroup.iloc[i, lst[j]].delete(0, len(widgetGroup.iloc[i, lst[j]].get()))
-            widgetGroup.iloc[i, lst[j]].insert(0, info.iloc[i, j])
-
-
-# respond to fileBtn, change the entries
-def fileCom():
-    path = getFile()
+                                                    ("all files", "*.*")),
+                                         defaultextension='.xlsx')
     pathLbl.config(text="File opened: " + path)
+    # noinspection PyArgumentList
     info = pd.read_excel(path, index_col=0)
-    changeEty(info)
+    for sbj in sbj_list:
+        for attrs in attrs_list:
+            attrs_entry_group.loc[sbj, attrs].delete(0, len(attrs_entry_group.loc[sbj, attrs].get()))
+            attrs_entry_group.loc[sbj, attrs].insert(0, info.loc[sbj, attrs])
 
 
-# calculate GPA
-def calculate(info):
-    # standardize level and score,calculate subject GPA
-    gpa = []
-    for i in sbjList:
-        info.loc[i, "level"] = stdLv(info.loc[i, "level"])
-        info.loc[i, "score"] = stdScr(int(info.loc[i, "score"]))
-        gpa.append(findGpa(i, info.loc[i, "level"], info.loc[i, "score"]))
+def calculate_gpa(info):
+    for sbj in sbj_list:
+        info.loc[sbj, "level"] = standardize_level(info.loc[sbj, "level"])
+        info.loc[sbj, "score"] = standardize_score(int(info.loc[sbj, "score"]))
+        info.loc[sbj, "gpa"] = find_gpa(sbj, info.loc[sbj, "level"], info.loc[sbj, "score"])
+        info.loc[sbj, "credit"] = float(info.loc[sbj, "credit"])
 
-    info.loc[:, "gpa"] = gpa  # input gpa into info dataframe
+    gpa_sum = np.sum(np.array(info["gpa"]) * np.array(info["credit"]))
+    credit_sum = np.sum(info["credit"])
 
-    # final result
-    gpaSum = 0
-    creditSum = 0
-    for i in sbjList:
-        info.loc[i, "credit"] = float(info.loc[i, "credit"])
-        gpaSum += info.loc[i, "gpa"] * info.loc[i, "credit"]
-        creditSum += info.loc[i, "credit"]
-
-    gpa = gpaSum / creditSum
-    gpa = int(gpa * 1000) / 1000
-    return gpa
+    return np.round(gpa_sum / credit_sum, 3)
 
 
-def getInput():
-    info = pd.DataFrame({}, index=sbjList, columns=["level", "score", "credit"])
-    lst = []
-    for i in range(len(widgetGroup.columns)):
-        if widgetGroup.columns[i][-3:] == "Ety":
-            lst.append(i)
-    for i in range(len(widgetGroup.index)):
-        for j in range(len(lst)):
-            info.iloc[i, j] = widgetGroup.iloc[i, lst[j]].get()
+def get_input():
+    info = pd.DataFrame({}, index=sbj_list, columns=attrs_list + ['gpa'])
+    for sbj in sbj_list:
+        for attrs in attrs_list:
+            info.loc[sbj, attrs] = attrs_entry_group.loc[sbj, attrs].get()
     return info
 
 
-# respond to calcBtn, calculate and output GPA
-def calcCom():
-    info = getInput()
-    gpa = calculate(info)
-    outLbl.config(text="Your GPA: " + str(gpa))
+def calculate_gpa_command():
+    info = get_input()
+    gpa = calculate_gpa(info)
+    output_label.config(text="Your GPA: " + str(gpa))
 
 
-# respond to saveInputBtn, save input
-def saveInputCom():
-    info = getInput()
-    path = tk.filedialog.asksaveasfilename(initialdir=".\\",
-                                           title="Save as Excel file",
+def save_input_command():
+    info = get_input().loc[:, :'credit']
+    path = tk.filedialog.asksaveasfilename(title="Save as Excel file",
                                            filetypes=(("Microsoft Excel", "*.xlsx"),
                                                       ("all files", "*.*")),
-                                           defaultextension=(("Microsoft Excel", "*.xlsx"),
-                                                             ("all files", "*.*")))
+                                           defaultextension='.xlsx')
     info.to_excel(path)
 
 
-# respond to saveGpaBtn, save GPA
-def saveGpaCom():
-    gpa = outLbl["text"].split(": ")[1]
+def save_gpa_command():
+    gpa = output_label["text"].split(": ")[1]
     fout = open("GPA output.txt", "a")
     t = time.strftime("%Y-%m-%d")
     fout.write(t + " GPA: " + gpa + "\n")
     fout.close()
 
 
-# subject GPA dictionaries
 chineseGpa = pd.DataFrame({"H": {1: 4.3, 2: 4.0, 3: 3.7, 4: 3.4, 5: 3.1, 6: 2.8, 7: 2.4, 8: 0},
                            "S": {1: 4.2, 2: 3.9, 3: 3.6, 4: 3.3, 5: 3.0, 6: 2.7, 7: 2.3, 8: 0},
                            "III": {1: 4.1, 2: 3.8, 3: 3.5, 4: 3.2, 5: 2.9, 6: 2.6, 7: 2.2, 8: 0},
@@ -155,58 +118,44 @@ nonLanguageGpa = pd.DataFrame({"H": {1: 4.3, 2: 4.0, 3: 3.7, 4: 3.4, 5: 3.1, 6: 
                                "S+": {1: 4.15, 2: 3.85, 3: 3.55, 4: 3.25, 5: 2.95, 6: 2.65, 7: 2.25, 8: 0},
                                "S": {1: 4.0, 2: 3.7, 3: 3.4, 4: 3.1, 5: 2.8, 6: 2.5, 7: 2.1, 8: 0}})
 
-# list of subject names
-sbjList = ['Chinese', 'Math', 'English', 'History', 'Physics', 'Chemistry', 'Elective']
+sbj_list = ['Chinese', 'Math', 'English', 'History', 'Physics', 'Chemistry', 'Elective']
+attrs_list = ["level", "score", "credit"]
 
-# create and customize window
 root = tk.Tk()
 root.geometry("700x300")
 root.title("GPA calculator")
 
-# info entries
-widgetGroup = pd.DataFrame({}, index=sbjList,
-                           columns=["sbjLbl", "levelEty", "scoreEty", "creditEty", "levelLbl", "scoreLbl", "creditLbl"])
-for i in range(7):
-    widgetGroup.loc[sbjList[i], "sbjLbl"] = tk.Label(text=sbjList[i] + ": ")
-    widgetGroup.loc[sbjList[i], "levelEty"] = tk.Entry(root)
-    widgetGroup.loc[sbjList[i], "scoreEty"] = tk.Entry(root)
-    widgetGroup.loc[sbjList[i], "creditEty"] = tk.Entry(root)
+sbj_label_group = {}
+attrs_entry_group = pd.DataFrame({}, index=sbj_list,
+                                 columns=attrs_list)
+attrs_label_group = pd.DataFrame({}, index=sbj_list,
+                                 columns=attrs_list)
+for i in range(len(sbj_list)):
+    sbj_label_group[sbj_list[i]] = tk.Label(text=sbj_list[i] + ": ")
+    sbj_label_group[sbj_list[i]].grid(row=i, column=0)
+    for j in range(len(attrs_list)):
+        attrs_label_group.loc[sbj_list[i], attrs_list[j]] = tk.Label(root, text=attrs_list[j] + ': ')
+        attrs_label_group.loc[sbj_list[i], attrs_list[j]].grid(row=i, column=2 * j + 1, padx=5)
+        attrs_entry_group.loc[sbj_list[i], attrs_list[j]] = tk.Entry(root)
+        attrs_entry_group.loc[sbj_list[i], attrs_list[j]].grid(row=i, column=2 * j + 2)
 
-    widgetGroup.loc[sbjList[i], "levelLbl"] = tk.Label(text="level: ")
-    widgetGroup.loc[sbjList[i], "scoreLbl"] = tk.Label(text="score: ")
-    widgetGroup.loc[sbjList[i], "creditLbl"] = tk.Label(text="credit: ")
-
-    widgetGroup.loc[sbjList[i], "sbjLbl"].grid(row=i, column=0)
-    widgetGroup.loc[sbjList[i], "levelLbl"].grid(row=i, column=1, padx=5)
-    widgetGroup.loc[sbjList[i], "levelEty"].grid(row=i, column=2)
-    widgetGroup.loc[sbjList[i], "scoreLbl"].grid(row=i, column=3, padx=5)
-    widgetGroup.loc[sbjList[i], "scoreEty"].grid(row=i, column=4)
-    widgetGroup.loc[sbjList[i], "creditLbl"].grid(row=i, column=5, padx=5)
-    widgetGroup.loc[sbjList[i], "creditEty"].grid(row=i, column=6)
-
-# button to choose input file
-fileBtn = tk.Button(text="Browse files", command=fileCom)
+fileBtn = tk.Button(text="Browse files", command=open_input_file)
 fileBtn.grid(row=7, column=0, columnspan=2)
 
-# show current input file
 pathLbl = tk.Label(text="File opened: ")
 pathLbl.grid(row=7, column=2, columnspan=10, sticky="w")
 
-# calculate button
-calcBtn = tk.Button(text="calculate", command=calcCom)
+calcBtn = tk.Button(text="calculate", command=calculate_gpa_command)
 calcBtn.grid(row=8, column=0, columnspan=2, pady=2)
-root.bind("<Return>", lambda event: calcCom())
+root.bind("<Return>", lambda event: calculate_gpa_command())
 
-# output
-outLbl = tk.Label(text="Your GPA:")
-outLbl.grid(row=8, column=2, sticky="w")
+output_label = tk.Label(text="Your GPA:")
+output_label.grid(row=8, column=2, sticky="w")
 
-# save input
-saveInputBtn = tk.Button(text="Save input", command=saveInputCom)
-saveInputBtn.grid(row=9, column=2, columnspan=2, pady=2)
+save_input_button = tk.Button(text="Save input", command=save_input_command)
+save_input_button.grid(row=9, column=2, columnspan=2, pady=2)
 
-# save GPA
-saveGpaBtn = tk.Button(text="Save GPA", command=saveGpaCom)
-saveGpaBtn.grid(row=9, column=4, columnspan=2, pady=2)
+save_gpa_button = tk.Button(text="Save GPA", command=save_gpa_command)
+save_gpa_button.grid(row=9, column=4, columnspan=2, pady=2)
 
 root.mainloop()
